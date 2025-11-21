7098c99c2d086c989f3903008c00d6a9f587c608
Sanjeela rawat <rawatrosy673@gmail.com>
2025-11-21 10:43:42 +0530

    new file

---
+++ index.js
@@ -0,0 +1,16 @@
+// let color = ["jan","feb","march","april","may","june","july"];
+// color.sort();
+
+// let unt = Math.floor
+
+// let msg = "     hello world   ";
+// // msg.trim();
+//   let res =  msg.toUpperCase().trim();
+
+//    console.log(res);
+   
+
+// for( let i=0; i<=50; i--){
+//     console.log(i);
+    
+// }
